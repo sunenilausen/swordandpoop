9a4db40daa285882581982c12480a969dffbf944
Sune Nilausen <sunenilausen@gmail.com>
2017-12-06 18:58:46 +0100

    Marlons projekt

---
+++ Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1"))
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (-5, 0);
+	}
+}
